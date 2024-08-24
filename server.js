e2066d32678de63e4a3780af95836ef9ed14c0c1
Gabriel Oliveira <140670619+Lend0X@users.noreply.github.com>
2024-08-23 22:21:13 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -11,16 +11,15 @@ app.get('/', (req, res) => {
 app.get('/consulta-cep/:cep', async (req, res) => {
     const cep = req.params.cep; // Obtendo o CEP da URL
     const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
-    
+
     try {
         if(!cepRegex.test(cep)){
-         res.status(400).send("CEP inválido. Formato: XXXXX-XXX")
-         return
+            res.status(400).send("CEP inválido. Formato: XXXXX-XXX")
+            return
         }
         // Fazendo a requisição para a API do ViaCEP
         const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
         res.json(response.data); // Retorna os dados da resposta
-        
     } catch (error) {
         console.error('Erro ao fazer requisição:', error);
         res.status(500).send('Erro ao consultar o CEP');
@@ -29,5 +28,4 @@ app.get('/consulta-cep/:cep', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
-
 });
